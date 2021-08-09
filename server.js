7f93edc1ad0692205e672f265224d029106c45b8
Miguel Alicea <malicea0783@gmail.com>
2021-08-08 20:52:51 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
