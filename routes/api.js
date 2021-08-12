dd97ac2be94c5e2ea0214033c5f8f93001772115
Miguel Alicea <malicea0783@gmail.com>
2021-08-11 20:44:58 -0500

    reverted to original code

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/Workout.js");
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({}, (error, data) => {
@@ -21,6 +21,21 @@ router.post("/api/workouts", (req, res) => {
   });
 });
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 router.get("/api/workouts/range", (req, res) => {
   Workout.aggregate([
     { $addFields: { totalDuration: { $sum: `$exercises.duration` } } },
@@ -33,19 +48,4 @@ router.get("/api/workouts/range", (req, res) => {
     .catch((err) => res.json(err));
 });
 
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate(
-    params.id,
-    { $push: { exercises: body } },
-    { new: true, runValidators: true }
-  )
-    .then((workouts) => {
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
 module.exports = router;
-
--- server.js
+++ server.js
@@ -12,15 +12,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workoutdb",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
